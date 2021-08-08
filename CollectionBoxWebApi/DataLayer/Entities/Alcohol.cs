b5e03042c389d2ef3801ea23a58dd89a5ab19922
DavidMikutishvili <davidstandartx@gmail.com>
2021-08-08 10:03:05 +0300

    Init commit, DAL

---
+++ CollectionBoxWebApi/DataLayer/Entities/Alcohol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionBoxWebApi.DataLayer.Entities
+{
+    public class Alcohol
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Tags { get; set; }
+
+        public int CollectionId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
